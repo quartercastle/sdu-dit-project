340a47ad86d62f74a653c409585eb92ba916dbd3
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-11-03 19:46:43 +0100

    React client + cors polices

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req,res) => {
-    res.send('Welcome to our fantastic blog!');
+app.use(cors());
+
+let db = [{ id: 0, title: "Jens Hansen" },
+          { id: 1, title: "Hanne Jensen" },
+          { id: 2, title: "Will Williamson"}];
+
+
+app.get('/api/posts', (req,res) => {
+    res.send(db);
 } 
 );
 
