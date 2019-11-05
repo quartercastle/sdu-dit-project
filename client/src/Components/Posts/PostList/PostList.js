c8825514b0a4cbefb0fe01045780a9c20116ec29
Ulrik Sandberg <UJSandberg@gmail.com>
2019-11-05 18:11:08 +0100

    Added post list conditional rendering logic

--- client/src/Components/Posts/PostList/PostList.js
+++ client/src/Components/Posts/PostList/PostList.js
@@ -1,28 +1,60 @@
 import React, { Component } from "react";
 import "./PostList.css";
+import { fetchPosts } from "../../api/backend";
 
 export default class PostList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      isLoading: false
     };
   }
   componentDidMount() {
     this.getPosts();
   }
 
-  getPosts = () => {
-    fetch("/api/posts")
-      .then(res => res.json())
-      .then(list => this.setState({ list }));
+  getPosts = async () => {
+    var res = await fetchPosts();
+
+    this.setState({ posts: res });
   };
+
+  renderContent = () => {
+    if (this.state.isLoading) {
+      return (
+        <div className="spinnerContainer">
+          <div className="loader"></div>
+          <div>Loading...</div>
+        </div>
+      );
+    } else {
+      return this.renderList();
+    }
+  };
+
+  renderList = () => {
+    console.log(this.state.posts);
+    if (this.state.posts.length > 0) {
+      return (
+        <div className="postContainer">
+          {this.state.posts.map(v => {
+            return <div>{v.author}</div>;
+          })}
+        </div>
+      );
+      return;
+    } else {
+      return (
+        <div className="spinnerContainer">
+          <img className="failedImg" src="/SWW.jpeg"></img>
+        </div>
+      );
+    }
+  };
+
   render() {
     const { posts } = this.state;
-    return (
-      <div className="App">
-        <h1>Welcome to our lovely blog!</h1>
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
--- client/src/Components/api/backend.js
+++ client/src/Components/api/backend.js
@@ -4,37 +4,31 @@ const axios = Axios.create({
   baseURL: "http://localhost:8000"
 });
 
-const fetchBlogPosts = async (page, pageSize) => {
+const fetchPosts = async (page, pageSize) => {
   //var res = await axios.get("/api/posts?");
 
-  const posts = [
+  return [
     {
       id: "",
-      author: "",
-      title: "",
-      description: "",
+      author: "Ulle",
       content: ""
     },
     {
       id: "",
-      author: "",
-      title: "",
-      description: "",
+      author: "Morten",
       content: ""
     }
   ];
 };
 
-const fetchBlogPost = async id => {
+const fetchPost = async id => {
   //var res = await axios.get("/api/post/id");
 
   return {
     id: "",
     author: "",
-    title: "",
-    description: "",
     content: ""
   };
 };
 
-export { fetchBlogPosts, fetchBlogPost };
+export { fetchPosts, fetchPost };
