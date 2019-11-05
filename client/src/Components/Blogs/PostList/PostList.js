40e51ae7b64a6e83461b10bb5bedbe97b21e0925
Ulrik Sandberg <UJSandberg@gmail.com>
2019-11-05 17:18:21 +0100

    Renamed BlogList to postlist

---
+++ client/src/Components/Blogs/PostList/PostList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./PostList.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = () => {
+    fetch("/api/posts")
+      .then(res => res.json())
+      .then(list => this.setState({ list }));
+  };
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+        <h1>Welcome to our lovely blog!</h1>
+      </div>
+    );
+  }
+}
