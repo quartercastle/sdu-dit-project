6e4708837a8f72c1e42f1edd47decd5a354339bb
Morten Skovgaard Jensen <mojen17@student.sdu.dk>
2019-11-06 20:59:55 +0100

    Changed startpage and popup

--- client/src/Components/Posts/PostList/Popup/Popup.js
+++ client/src/Components/Posts/PostList/Popup/Popup.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Popup from "reactjs-popup";
 import { createPost } from "../../../api/backend";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import "./Popup.css";
 
 export default class Pupup extends Component {
@@ -22,18 +24,26 @@ handleSubmit = () => {
   render() {
     return (
       <div className="createPostPop">
-        <Popup trigger={<button> Create new post</button>} modal>
+        <Popup trigger={<Button> Create new post</Button>} modal>
         {close => (
-        <div>
-          <div>
-            <label>Author: </label>
-            <input type="text" onChange={this.onAuthorInput} value={this.state.author} name="author" id="postAuthor"></input>
-          </div>
-          <div>
-            <label>Post: </label>
-            <textarea onChange={this.onPostInput} value={this.state.post} name="post" id="post"></textarea>
-          </div>
-            <button type="submit" onClick={this.handleSubmit} value="Submit" id="submit">Post</button>
+        <div className='popWindow'>
+                <div>
+                    <TextField type="text" label="Title"onChange={this.onAuthorInput} value={this.state.author} name="author" id="postAuthor"></TextField>
+                </div>
+            <div>
+            <TextField
+                id="standard-multiline-flexible"
+                label="Post"
+                multiline
+                rowsMax="4"
+                value={this.state.post}
+                onChange={this.onPostInput}  
+                margin="normal"
+            />
+            </div>
+            <div className='postBtnContainer'>
+                <Button id='postBtn' onClick={this.handleSubmit} value="Submit" >Post</Button>
+            </div>
         </div>
     )}
       </Popup>
