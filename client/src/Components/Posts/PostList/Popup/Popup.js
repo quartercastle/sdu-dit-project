daaf9328376c6a80246cbe514af4d8e6fc243212
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-11-07 21:21:28 +0100

    type fixed

--- client/src/Components/Posts/PostList/Popup/Popup.js
+++ client/src/Components/Posts/PostList/Popup/Popup.js
@@ -5,7 +5,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import "./Popup.css";
 
-export default class Pupup extends Component {
+export default class Popup extends Component {
   constructor(props) {
     super(props);
     this.state = { author: "", post: "" };
