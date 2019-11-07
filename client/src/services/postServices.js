1b6a7a8871bc7e7b4e9ebef49018fb362dfa932b
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-11-07 11:47:21 +0100

    services for post implemented

--- client/src/services/postServices.js
+++ client/src/services/postServices.js
@@ -3,12 +3,20 @@
 import axios from 'axios';
 
 export default {
-  //TODO add the rest of the services
-  create: async () => {
-    let res = await axios.post(`/api/post`, {
-      author: 'Fred',
-      content: 'Flintstone'
-    });
+  getAll: async () => {
+    let res = await axios.get(`/api/post`);
+    return res.data || [];
+  },
+  createPost: async (data) => {
+    let res = await axios.post(`/api/post`, data);
+    return res.data || [];
+  },
+  updatePost: async (id, data) => {
+    let res = await axios.post(`/api/post/${id}`, data);
+    return res.data || [];
+  },
+  deletePost: async (id) => {
+    let res = await axios.delete(`/api/post/${id}`);
     return res.data || [];
   }
   
