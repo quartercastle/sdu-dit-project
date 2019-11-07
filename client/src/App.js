2a517b38c8230e082d6abd68b9dcc923455dc243
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-11-07 11:46:57 +0100

    Removed test requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import postServices from './services/postServices';
 
 export default class App extends Component{
 
   componentDidMount(){
-    this.getProducts()
   }
 
-  getProducts = async () => {
-    let res = await postServices.create();
-  }
   
 render() {
   return (
